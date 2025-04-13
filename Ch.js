f22eec5727ddb037925fd0de17df31ab54d15bb6
M-KE24 <mikeangelosumido1993@gmail.com>
2025-04-13 19:18:09 +0800

    Create Ch.js

---
+++ Ch.js
@@ -0,0 +1,40 @@
+        manifestUri: 'https://tv.jomarhost.xyz/80bf754f-d8ff-4936-8409-451a921f95be.m3u8',
+        type: 'hls',
+		logo: 'https://i.imgur.com/C7a9W27.png',
+	        category: "Local"
+         },	 
+         {
+name: 'GMA',
+        manifestUri: 'https://gma.fflame143.workers.dev/master.m3u8',
+        type: 'hls',
+		logo: 'https://i.imgur.com/yh8g68M.png',
+	    category: "Local"
+         },	 
+         {
+name: 'GTV',
+        manifestUri: 'https://gtv.fflame143.workers.dev/master.m3u8',
+        type: 'hls',
+		logo: 'https://i.imgur.com/PsyP7Ga.jpeg',
+		 category: "Local"
+         },	 
+         {
+name: 'NET 25',
+        manifestUri: 'https://net25.fflame143.workers.dev/master.m3u8',
+        type: 'hls',
+		logo: 'https://i.imgur.com/2pRBUrH.png',
+		category: "Local"
+         },	 
+         {
+name: 'ALLTV 2',
+        manifestUri: 'https://alltv.fflame143.workers.dev/master.m3u8',
+        type: 'hls',
+		logo: 'https://i.imgur.com/YRA5Vz4.jpeg',
+		category: "Local"
+	      },
+  {
+        name: 'HallyPop',
+        logo: 'https://upload.wikimedia.org/wikipedia/commons/f/fb/GMA_Hallypop_logo.png',
+        type: 'hls',
+        manifestUri: 'https://jungotvstream.chanall.tv/jungotv/hallypop/stream.m3u8',
+        category: "Local"
+      },
