6c8fef02d5311625367d548d5058f691eed85c35
M-KE24 <mikeangelosumido1993@gmail.com>
2025-04-15 00:50:54 +0800

    Update scripthome.js

--- scripthome.js
+++ scripthome.js
@@ -763,6 +763,20 @@ name: 'ALLTV 2',
           slider.appendChild(card);
         });
       });
+	    
+	// For the Windows OS type the following command in cmd
+    "build": "set \"GENERATE_SOURCEMAP=false\" && react-scripts build"
+    
+    // For Linux OS
+    "build": "GENERATE_SOURCEMAP=false react-scripts build"
+     build": "cross-env GENERATE_SOURCEMAP=false react-scripts build",    
+       "scripts": {
+    "build": "cross-env NODE_ENV=production OTHERFLAG=myValue webpack --config build/webpack.config.js"
+    } 
+	        var myElement = document.getElementById("myElement");
+    myElement.addEventListener("contextmenu", function(event) {
+        event.preventDefault(); // Prevent the default right-click menu behavior
+    }); 
 
       loadStream(streams[0]); // Autoplay the first channel
     }
